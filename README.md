8dbb9ebd9fa63a93b97eb39cdffc88fc2e6c25fb
Tinson Lai <laitingsheng@hotmail.com>
2020-09-16 17:28:37 +0930

    Finalised script

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Automated-VCC-Finding-Script
+# Automated VCC Finding Script
+
+This is part of the Assignment 2 for course COMP SCI 7412 Secure Software Engineering.
+
+I assume that at least Python 3.8 (latest stable version) will be used for this project as there is no version requirement was imposed for this assignment
+SyntaxError may raise if using an earlier version due to the following most recent new features:
+
+* Type Hint (weak typing support): based on PEP 484, available from Python 3.5
+* f-string (string interpolation): based on PEP 498, available from Python 3.6
+* Walrus Operator (assignment expression): based on PEP 572, available from Python 3.8
+
+The heuristics implemented in the is based on _[VCCFinder: Finding Potential Vulnerabilities in Open-Source Projects to Assist Code Audits](https://dl.acm.org/doi/10.1145/2810103.2813604)_.
+
+It's important to run `/usr/bin/env python3 -m pip install -r requirements.txt` before executing the script.
+
+`repo.json` is a configuration file to be read by the script.
--- script.py
+++ script.py
@@ -3,28 +3,26 @@
 
 __author__ = "Tinson Lai"
 __copyright__ = "Copyright © 2020 Tinson Lai"
-__notes__ = """
-I assume that Python 3.8 (latest stable version) will be used for this project as there is no version requirement was imposed for this assignment
-SyntaxError may raise if using an earlier version due to the following most recent new features:
-    Type Hint (weak typing support): based on PEP 484, available from Python 3.5
-    f-string (string interpolation): based on PEP 498, available from Python 3.6
-    Walrus Operator (assignment expression): based on PEP 572, available from Python 3.8
-"""
 
 
+import re
 import json
 
 from git import InvalidGitRepositoryError, NoSuchPathError, Repo
 from multiprocessing import Pool
+from operator import methodcaller
 from optparse import OptionParser
 from pathlib import Path
 from shutil import rmtree
 from sys import stderr
 from typing import Any, Dict
+from unidiff import PatchSet
 
 
 class _descriptor:
     __slots__ = "_cve_id", "_files", "_fix", "_git", "_repo"
+
+    _ignored_patterns = re.compile(R"^[ \t]*/(?:\*|/).*$"), re.compile(R"^[ \t]*\*/?.*$")
 
     def __init__(self, repo_dir: Path, values: Dict[str, Any], force=False):
         for k, v in values.items():
@@ -51,9 +49,44 @@ class _descriptor:
 
     __repr__ = __str__
 
-    def analyse(self):
-        for file in self._files:
-            print(self._git.blame(self._fix, file))
+    def vcc_heuristic(self):
+        blames = {}
+        for parent_commit in (commit := self._git.commit(self._fix)).parents:
+            patches = PatchSet(self._git.git.diff(parent_commit, commit, *self._files, color="never"))
+            for patch, actual_file in zip(patches, self._files):
+                if f"a/{actual_file}" != patch.source_file or f"b/{actual_file}" != patch.target_file:
+                    raise ValueError("inconsistent file names in patch")
+                for hunk in patch:
+                    prev_line = None
+                    blamed_linenos = set()
+                    for line in hunk:
+                        if not any(p.match(line.value) for p in self._ignored_patterns):
+                            if line.is_removed:
+                                blamed_linenos.add(line.source_line_no)
+                            elif line.is_added:
+                                if prev_line and prev_line.is_context:
+                                    blamed_linenos.add(prev_line.source_line_no)
+                            elif line.is_context:
+                                if prev_line and prev_line.is_added:
+                                    blamed_linenos.add(line.source_line_no)
+                        prev_line = line
+                    for lineno in blamed_linenos:
+                        blamed = self._git.blame(parent_commit, actual_file, L=f"{lineno},{lineno}")
+                        if len(blamed) > 1:
+                            raise ValueError("blaming a single line yields multiple results")
+                        blamed_commit, _ = blamed[0]
+                        blamed_commit_str = str(blamed_commit)
+                        blames[blamed_commit_str] = blames.get(blamed_commit_str, 0) + 1
+
+        mv = 0
+        record = []
+        for k, v in blames.items():
+            if v > mv:
+                record = [k]
+                mv = v
+            elif v == mv:
+                record.append(k)
+        return record
 
 
 if __name__ == "__main__":
@@ -75,8 +108,8 @@ if __name__ == "__main__":
     with Pool() as pool:
         all_repo = pool.map(_new_repo, repo_config)
 
-        for job in [pool.apply_async(repo.analyse) for repo in all_repo]:
-            job.wait()
+        for repo, vcc in zip(all_repo, pool.map(methodcaller('vcc_heuristic'), all_repo)):
+            print(f"{repo}: {vcc}")
 
     # all_repo = (
     #     vulnerability_description(repo_dir, "CVE-2018-1000616", "opennetworkinglab", "onos", "af1fa39a53c0016e92c1de246807879c16f507d6", options.force),
